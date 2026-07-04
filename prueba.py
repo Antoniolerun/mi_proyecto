888d1bf6e5fcc17c2b874e2e37054fc1136a5ca5
Antonio Salas <antoniolerun@gmail.com>
2026-07-04 17:33:10 -0600

    Tercer commit: ok

--- prueba.py
+++ prueba.py
@@ -1,1 +1,4 @@
 print("Es una primera prueba de código en Python.")
+print("¡Hola, mundo!")
+print("Este es un ejemplo de cómo imprimir texto en la consola.")
+print("Python es un lenguaje de programación muy popular y versátil.")
